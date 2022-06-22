ec41aee64b8a39878bb1c1f1640df0a26c63b09f
Rodrigo Gomes do Nascimento <rodrigo@doocacommerce.com.br>
2022-06-22 20:38:42 -0300

    Task: Implementado classes request crud Group

---
+++ src/Providers/Requests/Group/GroupDeleteRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DoocaCommerce\Integrator\Providers\Requests\Group;
+
+use DoocaCommerce\Integrator\Providers\Requests\Request;
+
+class GroupDeleteRequest implements Request
+{
+    protected int $id;
+
+    public function __construct(
+        int $id
+    ) {
+        $this->id = $id;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            "id" => $this->id
+        ];
+    }
+
+    public function getMethod(): string
+    {
+        return 'POST';
+    }
+
+    public function getPath(): string
+    {
+        return 'integrator/import/group';
+    }
+
+    public function getBody(): array
+    {
+        return $this->toArray();
+    }
+
+    public function getHeader(): array
+    {
+        return [
+            'dc-action'  => 'delete'
+        ];
+    }
+}
